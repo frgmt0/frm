0bf537c63bb109b322aad8747bcb367f307c0205
Jason Weiss <jason.awz2005@icloud.com>
2024-12-22 23:53:34 -0800

    revert due to some fatal errors Revert "3.0.1"
    This reverts commit 104d1f1f8340c40cb84b7070894e08cb3e6d0f27.

--- src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Database = void 0;
-var database_1 = require("./core/database");
-Object.defineProperty(exports, "Database", { enumerable: true, get: function () { return database_1.Database; } });
